81dd4d5b598be5c446badf4e2edd33dfa87a1574
yayaeiei <72595823+yayaeiei@users.noreply.github.com>
2020-10-09 13:56:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 def index():
     a=os.environ['Authorization']
     try:
-        f = open("student.csv", "r")
-        for line in f.readlines():
+#        f = open("student.csv", "r")
+#        for line in f.readlines():
 #            print(line)
-            a = line.split(",")
-            if(a[0]=="21007"):
-                return a[4]
-        f.close()
+#            a = line.split(",")
+#           if(a[0]=="21007"):
+#                return a[4]
+#       f.close()
     except Exception:
         return "Could not read to file"
     
-    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/"
+    return "นางสาวอนัญญา บุญญา เลขที่ 16 ชั้น ม.4/15"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
