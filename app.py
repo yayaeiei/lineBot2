4c8142e6f20cb1e42addab80145d3e9ec50cb9dd
yayaeiei <72595823+yayaeiei@users.noreply.github.com>
2020-10-09 14:21:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def index():
     a=os.environ['Authorization']
     try:
-#        f = open("student.csv", "r")
-#       for line in f.readlines():
-#            print(line)
-#           a = line.split(",")
-#           if(a[0]=="21007"):
-#               return a[4]
-#       f.close()
+        f = open("student.csv", "r")
+       for line in f.readlines():
+            print(line)
+           a = line.split(",")
+           if(a[0]=="21007"):
+               return a[4]
+       f.close()
     except Exception:
         return "Could not read to file"
     
